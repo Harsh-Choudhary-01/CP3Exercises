217ff83ab35d3126a36fdc7739060f8929871005
Harsh-Choudhary-01 <harshch000@gmail.com>
2018-02-20 08:22:53 -0600

    Started Ancient Messages

---
+++ src/Graphs/Sec4_2/AncientMessages.java
@@ -0,0 +1,11 @@
+package Graphs.Sec4_2;
+
+import java.util.*;
+import java.io.*;
+
+class AncientMessages {
+    public static void main(String[] args) throws IOException {
+        //Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(new File("data/AncientMessages.txt"));
+    }
+}
