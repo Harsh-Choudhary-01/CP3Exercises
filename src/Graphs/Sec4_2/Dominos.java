4ba2d4f82627a47dce3d58abaad4ea62f3beaeaa
Harsh-Choudhary-01 <harshch000@gmail.com>
2018-02-28 12:09:45 -0600

    Finished Dominos

--- src/Graphs/Sec4_2/Dominos.java
+++ src/Graphs/Sec4_2/Dominos.java
@@ -3,13 +3,19 @@ package Graphs.Sec4_2;
 import java.util.*;
 import java.io.*;
 
+/*
+Errors:
+1. My code was correct from the beginning, the only errors I had was a stack overflow.
+I tried to fix this error by removing local variables in the recursive function but it
+didn't help. In order to solve the problem I changed the code to an iterative algorithm.
+ */
 class Dominos {
     static int[] dfs_num;
     static int[] dfs_low;
     static boolean[] visited;
-    static ArrayList<ArrayList<Integer>> adjList;
+    static ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
     static ArrayList<ArrayList<Integer>> cc = new ArrayList<>();
-    static Stack<Integer> stack;
+    static Stack<Integer> visitedStack = new Stack<>();
     static int counter;
     public static void main(String[] args) throws IOException {
         //Scanner in = new Scanner(System.in);
@@ -18,9 +24,9 @@ class Dominos {
         for (int time = 1; time <= times; time++) {
             int n = in.nextInt();
             int edges = in.nextInt();
-            adjList = new ArrayList<>();
+            adjList.clear();
             cc.clear();
-            stack = new Stack<>();
+            visitedStack.clear();
             for (int i = 1; i <= n; i++)
                 adjList.add(new ArrayList<>());
             dfs_low = new int[n];
@@ -36,33 +42,62 @@ class Dominos {
             for (int i = 0; i < n; i++) {
                 if (dfs_num[i] == -1) {
                     counter = 0;
-                    tarjanScc(i);
+                    iterativeScc(i);
+                }
+            }
+            System.out.println(cc.size());
+        }
+    }
+    
+    static void iterativeScc(int start) {
+        Stack<Entry> stack = new Stack<>();
+        stack.push(new Entry(start, true));
+        while (!stack.isEmpty()) {
+            Entry entry = stack.pop();
+            int u = entry.rootVertex;
+            if (entry.firstCall) {
+                visitedStack.push(u);
+                entry.firstCall = false;
+                stack.push(entry);
+                dfs_num[u] = dfs_low[u] = counter++;
+                visited[u] = true;
+                for (int i = 0; i < adjList.get(u).size(); i++)
+                    if (dfs_num[adjList.get(u).get(i)] == -1)
+                        stack.push(new Entry(adjList.get(u).get(i), true));
+            }
+            else {
+                for (int i = 0; i < adjList.get(u).size(); i++) {
+                    if (visited[adjList.get(u).get(i)])
+                        dfs_low[u] = Math.min(dfs_low[u], dfs_low[adjList.get(u).get(i)]);
+                    else {
+                        for (int j = cc.size() - 1; j >= 0; j--) {
+                            if (cc.get(j).contains(adjList.get(u).get(i)))
+                                cc.remove(j);
+                        }
+                    }
+                }
+                if (dfs_num[u] == dfs_low[u]) {
+                    ArrayList<Integer> seq = new ArrayList<>();
+                    cc.add(seq);
+                    while (true) {
+                        int pop = visitedStack.pop();
+                        seq.add(pop);
+                        visited[pop] = false;
+                        if (pop == u)
+                            break;
+                    }
                 }
             }
         }
     }
-
-    static void tarjanScc(int u) {
-        dfs_num[u] = dfs_low[u] = counter++;
-        stack.push(u);
-        visited[u] = true;
-        for (int i =  0; i < adjList.get(u).size(); i++) {
-            int neighbor = adjList.get(u).get(i);
-            if (dfs_num[neighbor] == -1)
-                tarjanScc(neighbor);
-            if (visited[neighbor])
-                dfs_low[u] = Math.min(dfs_low[u], dfs_low[neighbor]);
-        }
-        if (dfs_num[u] == dfs_low[u]) {
-            ArrayList<Integer> seq = new ArrayList<>();
-            cc.add(seq);
-            while (true) {
-                int pop = stack.pop();
-                seq.add(pop);
-                visited[pop] = false;
-                if (pop == u)
-                    break;
-            }
+    
+    static class Entry {
+        int rootVertex;
+        boolean firstCall; //If false returning after recursive
+        
+        public Entry(int rootVertex, boolean firstCall) {
+            this.rootVertex = rootVertex;
+            this.firstCall = firstCall;
         }
     }
 }
